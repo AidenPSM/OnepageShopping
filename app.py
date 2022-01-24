c59833ebf566ec91c9d70eb2ebacdfdb8abc06a6
AidenPSM <aidenpsmc@gmail.com>
2022-01-23 21:08:16 -0400

    Done

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
+db = client.dbhomework
+
+
+## Show HTML
+@app.route('/')
+def homework():
+    return render_template('index.html')
+
+
+# Order(POST) API
+@app.route('/order', methods=['POST'])
+def save_order():
+    name_receive = request.form['name_give']
+    amount_receive = request.form['amount_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+
+    doc = {
+        'name': name_receive,
+        'amount': amount_receive,
+        'address': address_receive,
+        'phone': phone_receive
+    }
+    db.orders.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': 'Order Complete!'})
+
+
+# Order List(Read) API
+@app.route('/order', methods=['GET'])
+def view_orders():
+    orders = list(db.orders.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'orders': orders})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
